334d915d30287fff1a7724916151b0970c86e5e8
mgenius3 <benmos16@gmail.com>
2023-11-10 13:31:08 +0000

    hello

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const next = require("next");
 const dotenv = require("dotenv");
 const dev = process.env.NODE_ENV !== "production";
+const path = require("path");
 
 dotenv.config();
 const app = next({ dev });
@@ -10,49 +11,51 @@ const port = process.env.PORT || 3000;
 const errorHandler = require("./middleware/errorHandler");
 const morgan = require("morgan");
 
-app.prepare().then(() => {
-  const server = express();
+// app.prepare().then(() => {
 
-  server.use(morgan("dev"));
+const server = express();
 
-  // Middleware to parse JSON request body
-  server.use(express.json({ limit: "10mb" }));
-  server.use("/v1/driver", require("./routes/driver"));
-  server.use("/v1/admin", require("./routes/admin"));
+server.use(morgan("dev"));
 
-  // error handling
-  server.use(errorHandler);
+server.use(express.static(path.join(__dirname, "out")));
+server.use("/styles", express.static(path.join(__dirname, "/styles")));
 
-  // Default route handler
-  server.get("*", (req, res) => {
-    return handle(req, res);
+server.use(express.json({ limit: "10mb" }));
+server.use("/v1/driver", require("./routes/driver"));
+server.use("/v1/admin", require("./routes/admin"));
+
+// error handling
+server.use(errorHandler);
+
+// Default route handler
+// server.get("*", (req, res) => {
+//   return handle(req, res);
+// });
+
+// Middleware to handle requests that don't match any route
+server.use(function (req, res) {
+  res.status(404);
+
+  // Render a custom error page
+  res.json({
+    title: "Page Not Found",
+    message: "The requested page does not exist",
   });
+});
 
-  // Middleware to handle requests that don't match any route
-  server.use(function (req, res) {
-    res.status(404);
+// Error handler middleware
+server.use(function (err, req, res) {
+  console.error(err.stack);
+  res.status(500);
 
-    // Render a custom error page
-    res.json({
-      title: "Page Not Found",
-      message: "The requested page does not exist",
-    });
+  // render a custom error page
+  res.json({
+    title: "Server Error",
+    message: "Something went wrong on our end",
   });
+});
 
-  // Error handler middleware
-  server.use(function (err, req, res) {
-    console.error(err.stack);
-    res.status(500);
-
-    // render a custom error page
-    res.json({
-      title: "Server Error",
-      message: "Something went wrong on our end",
-    });
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+server.listen(port, (err) => {
+  if (err) throw err;
+  console.log(`> Ready on http://localhost:${port}`);
 });
