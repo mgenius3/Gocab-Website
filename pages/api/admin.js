475fd0b56d6bfde3067b24ea39fe6647b468030f
mgenius3 <benmos16@gmail.com>
2023-11-11 13:21:57 +0000

    hello

--- pages/api/admin.js
+++
@@ -1,9 +0,0 @@
-// In the ./pages/api/your-api-route-handler.js file (API route handler)
-import nextConnect from "next-connect";
-
-import router from "../next_routes/admin";
-
-const apiHandler = nextConnect();
-apiHandler.use(router);
-
-export default apiHandler;
