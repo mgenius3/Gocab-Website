e3ff0512d92137cb5ad2b1299d4e79acffd42f6c
mgenius3 <benmos16@gmail.com>
2023-11-13 21:32:41 +0000

    deploy

--- pages/routes/driver.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-
-const { DriverRegistration, DriverSignIn } = require("../controller/driver");
-
-router.post("/register", DriverRegistration);
-router.post("/login", DriverSignIn);
-
-module.exports = router;
